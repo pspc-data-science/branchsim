694973aee13048d1d6684881345e6a415aa942b8
David Maybury <david@maybury.ca>
2020-05-23 00:24:12 -0400

    Changed functionality of the renewal equation. Takes functions as arguments.

--- R/CMJ-functions.R
+++ R/CMJ-functions.R
@@ -4,9 +4,9 @@
 #' (lambda_chi, xi_chi, chi). See appendix C of Branching Processes in Biology by Kimmel and Axelrod for more details.
 #' Z = G(t) + int_0^t Z(t - u) mu(du)
 #'  
-#' @param dMu The function dMu(t) that defines mu(dt) = dMu(t) dt.
+#' @param dMu The function dMu(t) that defines mu(dt) = dMu(t) dt. Function as argument.
 #' @param G A function that that defines the expectation of the random characteristic of interest. Set to G=1 
-#' by default (total number born).
+#' by default (total number born). Function as argument.
 #' @param T The maximum of the time interval for integration 0,T.
 #' @param nstep The number of steps for the integration. 
 #' 
@@ -37,52 +37,57 @@ renewal_function<- function(dMu, G=1, T=100, nstep = 10000){
 
 #' A function for the infinitesimal expectation in births: mu(dt) = dMu(t) dt
 #'
-#' @param t A vector of time evaluations
-#' @param a The shape parameter of the gamma life time distribution. Default a =10
-#' @param b The rate parameter of the gamma life time distribution. Default b = 1
-#' @param lambda The arrival rate of infectious interactions. Default lambda = .11
-#' @param p The parameter of the logarithmic distribution for the number of infected during an event.
-#' Default p=0.5
+#' @param A The shape parameter of the gamma life time distribution. Default A =10
+#' @param B The rate parameter of the gamma life time distribution. Default B = 1
+#' @param Lambda The arrival rate of infectious interactions. Default Lambda = .11
+#' @param P The parameter of the logarithmic distribution for the number of infected during an event.
+#' Default P=0.5
 #' 
-#' @return Function evaluation at time vector t.
+#' @return A function of t: dMu(t).
 #'
 #' @export
-dMu<- function(t, a=10, b=1, lambda = .11, p = 0.5){
-  
-  r = -lambda/log(1-p)
-  K<- r*p/(1-p)
-  
-  gamma_fun<- function(x){
-    K*(1 - pracma::gammainc(b*x,a)[3])
+
+dMu<- function(A=10, B=1, Lambda = .11, P = 0.5){
+
+  dmu<- function(t, a=A, b=B, lambda = Lambda, p = P){
+    
+    r = -lambda/log(1-p)
+    K<- r*p/(1-p)
+    
+    gamma_fun<- function(x){
+      K*(1 - pracma::gammainc(b*x,a)[3])
+    }
+    
+    
+    result<- map_dbl(t, gamma_fun)
+    result[t==0]<-K
+    return(result)
+    
   }
-  
-  
-  result<- map_dbl(t, gamma_fun)
-  result[t==0]<-K
-  return(result)
-  
 }
 
-#' A function for the expecation for the number of idividuals existing in the process at moment t. 
-#' Used with the renewal eqaution, it will give the expectation for the instantaneous number infected 
+#' A function for the expecation for the number of individuals existing in the process at moment t. 
+#' Used with the renewal equation, it will give the expectation for the instantaneous number infected 
 #' as a function of time.
 #'
-#' @param t A vector of time evaluations
-#' @param a The shape parameter of the gamma life time distribution. Default a =10
-#' @param b The rate parameter of the gamma life time distribution. Default b = 1
+#' @param A The shape parameter of the gamma life time distribution. Default a =10
+#' @param B The rate parameter of the gamma life time distribution. Default b = 1
 #' 
-#' @return Function evaluation at time vector t.
+#' @return A function G(t) = 1 - F(t); F(t) is the life-time distribution.
 #'
 #' @export
-G<- function(t, a=10, b=1){
-  
-  gamma_fun<- function(x){
-    (1 - pracma::gammainc(b*x,a)[3] )
+G<- function(A=10, B=1){
+  g<- function(t, a=A, b=B){
+    
+    gamma_fun<- function(x){
+      (1 - pracma::gammainc(b*x,a)[3] )
+    }
+    
+    result<- map_dbl(t, gamma_fun)
+    result[t==0]<-1
+    return(result)
   }
-  
-  result<- map_dbl(t, gamma_fun)
-  result[t==0]<-1
-  return(result)
+
 }
 
 #' A function for obtaining the Malthusian parameter of the CMJ process. It also yel
